ae2f46105bc4c42de1394edcd791b43f4393c3e9
ismylsmylv <ismayilovsmyl2020@gmail.com>
2023-10-11 10:31:11 +0400

    unibank project updated, added script.js

---
+++ 11-UnibankCardsPage/script.js
@@ -0,0 +1,37 @@
+console.log("Hello, World!");
+
+var greet = "Hello, World!";
+console.log(greeting);
+
+let greet = "Hello";
+greet = greet + "World";
+console.log(greet);
+
+let text = "Hello, World!";
+console.log(typeof text);
+
+let number = 42;
+console.log(typeof number);
+
+let entity = true;
+console.log(typeof entity);
+
+let colors = ["red", "green", "blue"];
+console.log(typeof colors);
+
+let user = {
+    name: "David",
+    age: 23,
+};
+console.log(typeof user);
+
+example = null;
+console.log(typeof example);
+
+
+
+
+
+
+
+
