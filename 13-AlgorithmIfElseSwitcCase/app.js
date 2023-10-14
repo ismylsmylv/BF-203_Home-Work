78e528b2be7c99c8311f85c9bfe6a8f20ae80a6a
ismylsmylv <ismayilovsmyl2020@gmail.com>
2023-10-14 10:07:53 +0400

    12th homework added

---
+++ 13-AlgorithmIfElseSwitcCase/app.js
@@ -0,0 +1,75 @@
+// İstifadəçidən 3 ədəd alın. Bu ədədlərin üçbucağın tərəfi olub olmadığını yoxlayan proqram tərtib edin
+// let a = Number(prompt("Enter first side"))
+// let b = Number(prompt("Enter second side"))
+// let c = Number(prompt("Enter third side"))
+// if (a + b > c && b + c > a && a + c > b) {
+//     alert("This is triangle")
+// }
+// else {
+//     alert("This is not triangle")
+// }
+
+// İstifadəçidən 3 ədəd alın. Onların bərabərtərəfli üçbucaq olduğunu təyin edən proqram tərtib edin.
+// let a = Number(prompt("Enter first side"))
+// let b = Number(prompt("Enter second side"))
+// let c = Number(prompt("Enter third side"))
+// if (a == b && b == c && a == c) {
+//     alert("This is equilateral triangle")
+// }
+// else {
+//     alert("This is not equilateral triangle")
+// }
+
+// İstifadəçidən 2 ədəd alın. Hansının ən böyük olduğunu tapan proqram tərtib edin
+// let a = Number(prompt("Enter first number"))
+// let b = Number(prompt("Enter second number"))
+// if (a > b) {
+//     alert(`${a} is greater than ${b}`)
+// }
+// else {
+//     alert(`${b} is greater than ${c}`)
+// }
+
+// İstifadəçidən 3 ədəd alın. Hansının ən böyük olduğunu tərtib edin.
+// let a = Number(prompt("Enter first number"))
+// let b = Number(prompt("Enter second number"))
+// let c = Number(prompt("Enter third number"))
+// if (a > b && a > c) {
+//     alert(`${a} is greater`)
+// }
+// else if (b > a && b > c) {
+//     alert(`${b} is greater`)
+// }
+// else {
+//     alert(`${c} is greater`)
+// }
+
+// İstifadəçidən 3 ədəd alın. Onları artan sıra ilə düzən proqram tərtib edin.
+// let a = Number(prompt("Enter first number"));
+// let b = Number(prompt("Enter second number"));
+// let c = Number(prompt("Enter third number"));
+
+// if (a <= b && a <= c) {
+//     if (b <= c) {
+//         alert(`${a}, ${b}, ${c}`);
+//     } 
+//     else {
+//         alert(`${a}, ${c}, ${b}`);
+//     }
+// } 
+// else if (b <= a && b <= c) {
+//     if (a <= c) {
+//         alert(`${b}, ${a}, ${c}`);
+//     } 
+//     else {
+//         alert(`${b}, ${c}, ${a}`);
+//     }
+// } 
+// else {
+//     if (a <= b) {
+//         alert(`${c}, ${a}, ${b}`);
+//     } 
+//     else {
+//         alert(`${c}, ${b}, ${a}`);
+//     }
+// }
