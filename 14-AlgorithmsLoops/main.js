038e5fc90593971ac63720f6658742deaf58acf5
ismylsmylv <ismayilovsmyl2020@gmail.com>
2023-10-15 09:41:00 +0400

    loop algorithms completed

--- 14-AlgorithmsLoops/main.js
+++ 14-AlgorithmsLoops/main.js
@@ -252,10 +252,15 @@ let arr = [2, 9, -5, -4, "AzerBayCan", true, 12, "LANKARAN", "LimOn", 182, 4];
 
 for (let i = 0; i < arr.length; i++) {
     if (typeof arr[i] === 'string') {
-        let upCount = 0;
+        let count = 0;
 
-        //some stuff
-        if (upCount > 2) {
+        for (let j = 0; j < arr[i].length; j++) {
+            if (arr[i][j] >= 'A' && arr[i][j] <= 'Z') {
+                count++;
+            }
+        }
+
+        if (count > 2) {
             console.log(arr[i]);
         }
     }
