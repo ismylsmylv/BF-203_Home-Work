3250fd934653c84601aa174b7504118ed828eb91
ismylsmylv <ismayilovsmyl2020@gmail.com>
2023-11-01 00:31:24 +0400

    project created

---
+++ 26-WishlistWithLocalStorage/assets/script/cart.js
@@ -0,0 +1,137 @@
+let url = "https://fakestoreapi.com/products"
+let products = document.querySelector(".products")
+let product = document.querySelector(".product")
+let check = document.querySelector(".check")
+let productCount = document.querySelector(".productCount")
+let totalPrice = document.querySelector(".totalPrice")
+let details = document.querySelector(".details")
+let removeAll = document.querySelector(".removeAll")
+
+let quantity = 1
+let cartItemsAll = {}
+// fetch(url).then(res => res.json()).then(data => {
+//     data.forEach((element, i) => {
+//         products.innerHTML += `<div class="product">
+//         <img src="${element.image}" alt="">
+//         <h2 class="productName">${element.title}</h2>
+//         <div class="count">
+//             <button class="increase">
+//                 <p>+</p>
+//             </button>
+//             <p class="counter">2</p>
+//             <button class="decrease">
+//                 <p>-</p>
+//             </button>
+//         </div>
+//         <div class="prices">
+//             <h3 class="price">$${element.price}</h3>
+//             <button class="save">Save for later</button>
+//             <button class="remove">Remove</button>
+//         </div>
+//     </div>`
+//     });
+// })
+
+let cartItems = JSON.parse(localStorage.getItem("cartItems"))
+cartItems.forEach(element => {
+    let quantity = 1
+    products.innerHTML += `<div class="product">
+        <img src="${element.image}" alt="">
+        <h2 class="productName">${element.title}</h2>
+        <div class="count">
+            <button data-id="${element.id}" class="increase">
+                <p>+</p>
+            </button>
+            <p class="counter">2</p>
+            <button class="decrease">
+                <p>-</p>
+            </button>
+        </div>
+        <div class="prices">
+            <h3 class="price">$${element.price}</h3>
+            <button class="save">Save for later</button>
+            <button class="remove">Remove</button>
+        </div>
+    </div>`
+    totalPrice += element.price
+    details.innerHTML = `<div class="detailsLeft">
+    <h3>
+        Sub-Total
+    </h3>
+    <div class="productCount">${quantity} items</div>
+    </div>
+    <div class="detailsRight">
+    <h1 class="totalPrice">$${totalPrice}</h1>
+    </div>`
+
+
+})
+let save = document.querySelectorAll(".save")
+let remove = document.querySelector(".remove")
+let price = document.querySelector(".price")
+remove.addEventListener("click", function(){
+    totalPrice-=price.value
+})
+
+products.addEventListener("click", function (event) {
+    if (event.target.classList.contains("increase") || event.target.classList.contains("decrease")) {
+        const itemId = event.target.getAttribute("data-id");
+        const item = cartItemsAll[itemId];
+
+        if (event.target.classList.contains("increase")) {
+            item.quantity++;
+            totalPrice.textContent = `$${item.price * item.quantity}`;
+            productCount.textContent = `${item.quantity} items`;
+        } else {
+            if (item.quantity > 1) {
+                item.quantity--;
+                totalPrice.textContent = `$${item.price * item.quantity}`;
+                productCount.textContent = `${item.quantity} items`;
+            } else {
+                delete cartItemsAll[itemId];
+            }
+        }
+
+        localStorage.setItem("cartItems", JSON.stringify(Object.values(cartItemsAll)));
+    }
+});
+
+
+
+//checkout
+check.addEventListener("click", function () {
+    // productCount.textContent = "0";
+    // totalPrice.textContent = "$0";
+    products.innerHTML = "";
+    details.innerHTML = `<div class="detailsLeft">
+    <h3>
+        Sub-Total
+    </h3>
+    <div class="productCount">0 items</div>
+    </div>
+    <div class="detailsRight">
+    <h1 class="totalPrice">$0</h1>
+    </div>`
+    let cartItems = [];
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+});
+// remove all
+removeAll.addEventListener("click", function () {
+    // productCount.textContent = "0";
+    // totalPrice.textContent = "$0";
+    products.innerHTML = "";
+    details.innerHTML = `<div class="detailsLeft">
+    <h3>
+        Sub-Total
+    </h3>
+    <div class="productCount">0 items</div>
+    </div>
+    <div class="detailsRight">
+    <h1 class="totalPrice">$0</h1>
+    </div>`
+    let cartItems = [];
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+});
+// remove.addEventListener("click", function () {
+//     console.log(this);
+// })
---
+++ 26-WishlistWithLocalStorage/assets/script/index.js
@@ -0,0 +1,404 @@
+let body = document.querySelector("body")
+let products = document.querySelector(".products")
+let card = document.querySelector(".products")
+let url = "https://fakestoreapi.com/products/"
+let favs = document.querySelector(".favs")
+let cart = document.querySelector(".cart")
+let favBtn = document.querySelectorAll(".favBtn")
+let remBtn = document.querySelectorAll(".remBtn")
+let total = document.querySelector(".total")
+let favHead = document.querySelector(".favHead");
+let cartHead = document.querySelector(".cartHead");
+let cartElem = document.querySelector(".cartElem");
+let cartProd = document.querySelector(".cartProd");
+let navbar = document.querySelector(".navbar");
+let all = document.querySelector(".all");
+let men = document.querySelector(".men");
+let women = document.querySelector(".women");
+let elecs = document.querySelector(".elecs");
+let content = document.querySelector(".content");
+let acc = document.querySelector(".acc");
+let filters = document.querySelector("#filters");
+let lowToHighPrice = document.querySelector(".lowToHighPrice");
+let highToLowPrice = document.querySelector(".highToLowPrice");
+let lowToHighRating = document.querySelector(".lowToHighRating");
+let highToLowRating = document.querySelector(".highToLowRating");
+
+let hasAdded
+let sum = 0
+let mens = "men's clothing"
+let womens = "women's clothing"
+let jewelery = "jewelery"
+let electronics = "electronics"
+
+
+//fetch functions
+let preFetch = function () {
+    products.innerHTML = ""
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            creator(data);
+        }).catch()
+}
+let fetcher = function (category) {
+    products.innerHTML = ""
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            data.forEach((element, i) => {
+                if (element.category === category) {
+                    products.innerHTML += `<div class="card product" style="width: 18rem;">
+                        <img name="${i}" class="cardImage card-img-top" src="${element.image}" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">${element.title}</h5>
+                            <p class="card-text price "><b>$${element.price}</b></p>
+                            <div class="details">
+                                <p class="card-text category "><i class="fa-solid fa-bag-shopping"></i> ${element.category}</p>
+                                <p class="card-text rating "><i class="fa-solid fa-star"></i> ${element.rating?.rate}</p>
+                                <p class="card-text stock "><i class="fa-solid fa-truck"></i> ${element.rating?.count}</p>
+                            </div>
+                            <div class="btns mb-0">
+                                <button  name="${i}" type="button" class="favBtn btn btn-danger"><i class="fa-regular fa-heart"></i>
+                                    <p>Favorite</p>
+                                </button>
+                                <button type="button" name="${i}" class="addBtn btn btn-primary"><i class="fa-regular fa-cart-shopping"></i>
+                                    <p>Cart</p>
+                                </button>
+                            </div>
+                        </div>
+                    </div>`;
+                }
+            });
+            // adding to favorite
+            let favBtns = document.querySelectorAll(".favBtn");
+for (let btn of favBtns) {
+    btn.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        let selectedItem = data[btn.name];
+        let favItems = JSON.parse(localStorage.getItem("favItems")) || [];
+
+        let isAlreadyFav = favItems.some((item) => item.id === selectedItem.id);
+
+        if (!isAlreadyFav) {
+            favItems.push(selectedItem);
+            localStorage.setItem("favItems", JSON.stringify(favItems));
+
+            btn.querySelector("i").classList.remove("fa-regular");
+            btn.querySelector("i").classList.add("fa-solid");
+
+            Swal.fire({
+                icon: 'success',
+                title: `${selectedItem.title} added to favorites`,
+            });
+        } else {
+            Swal.fire({
+                icon: 'info',
+                title: 'Item is already in favorites',
+            });
+        }
+
+        console.log(favItems);
+    });
+}
+            // adding to cart
+            let quantity = 0
+            let addBtns = document.querySelectorAll(".addBtn");
+            for (let btn of addBtns) {
+                btn.addEventListener("click", function (e) {
+                    e.preventDefault();
+                    let selectedItem = data[btn.name];
+                    quantity++
+                    //save to local
+                    let cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+                    cartItems.push(selectedItem);
+                    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+
+
+                    Swal.fire({
+                        icon: 'success',
+                        title: `${selectedItem.title} added to cart`,
+                    });
+
+
+
+                    //removing from cart
+                    let remBtns = document.querySelectorAll(".remCartBtn");
+                    for (let btn of remBtns) {
+                        btn.addEventListener("click", function () {
+                            Swal.fire({
+                                title: 'Are you sure want to remove item from cart?',
+                                icon: 'warning',
+                                showCancelButton: true,
+                                confirmButtonColor: '#3085d6',
+                                cancelButtonColor: '#d33',
+                                confirmButtonText: 'Yes, remove item.'
+                            }).then((result) => {
+                                if (result.isConfirmed) {
+                                    Swal.fire({
+                                        icon: 'success',
+                                        text: 'Item removed'
+                                    }
+                                    )
+                                    sum -= Number(this.parentElement.querySelector('.price').textContent.replace('$', ''));
+                                    cartHead.innerHTML = `<h3>Cart: $${sum.toFixed(2)} </h3>`;
+                                    //   if(sum=="0.00"){
+                                    //     cartHead.innerHTML = `<h3>Cart</h3>`;
+                                    // }
+                                    this.parentElement.remove()
+
+                                }
+                            })
+
+
+
+                        })
+                    }
+
+                })
+            }
+
+            //info 
+            let cardClicks = document.querySelectorAll(".card");
+            cardClicks.forEach((card) => {
+                card.addEventListener("click", function (e) {
+                    e.preventDefault();
+                    if (!e.target.closest(".favBtn") && !e.target.closest(".addBtn")) {
+                        Swal.fire({
+                            title: data[this.querySelector(".addBtn").name].title,
+                            text: `${data[this.querySelector(".addBtn").name].description} Rated with ${data[this.querySelector(".addBtn").name].rating.rate}/5, you can get this item for $${data[this.querySelector(".addBtn").name].price} `,
+                            imageUrl: data[this.querySelector(".addBtn").name].image,
+                            imageWidth: 250,
+                            imageHeight: 200,
+                            imageAlt: 'Custom image',
+                        });
+                    }
+                });
+            });
+
+        }).catch()
+}
+let creator = function (data) {
+    data.forEach((element, i) => {
+        products.innerHTML += `<div class="card product" style="width: 18rem;">
+            <img name="${i}" class="cardImage card-img-top" src="${element.image}" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">${element.title}</h5>
+                <p class="card-text price "><b>$${element.price}</b></p>
+                <div class="details">
+                    <p class="card-text category "><i class="fa-solid "></i> ${element.category}</p>
+                    <p class="card-text rating "><i class="fa-solid fa-star"></i> ${element.rating?.rate}</p>
+                    <p class="card-text stock "><i class="fa-solid fa-truck"></i> ${element.rating?.count}</p>
+                </div>
+                <div class="btns mb-0">
+                    <button  name="${i}" type="button" class="favBtn btn btn-danger"><i class="fa-regular fa-heart"></i>
+                        <p>Favorite</p>
+                    </button>
+                    <button type="button" name="${i}" class="addBtn btn btn-primary"><i class="fa-solid fa-cart-shopping"></i>
+                        <p>Cart</p>
+                    </button>
+                </div>
+            </div>
+        </div>`;
+
+    });
+    // adding to favorite
+    let favItems
+    let favBtns = document.querySelectorAll(".favBtn");
+    for (let btn of favBtns) {
+        btn.addEventListener("click", function (e) {
+            e.preventDefault();
+            hasAdded = btn.querySelector("i");
+    
+            // add
+            if (hasAdded.classList.contains("fa-regular")) {
+                let selectedItem = data[btn.name];
+                let favItems = JSON.parse(localStorage.getItem("favItems")) || [];
+                favItems.push(selectedItem);
+                localStorage.setItem("favItems", JSON.stringify(favItems));
+                hasAdded.classList.remove("fa-regular");
+                hasAdded.classList.add("fa-solid");
+                Swal.fire({
+                    icon: 'success',
+                    title: `${selectedItem.title} added to favorites`,
+                });
+            } else {
+                //remove
+                let selectedItem = data[btn.name];
+                let favItems = JSON.parse(localStorage.getItem("favItems")) || [];
+                let removeIndex = favItems.findIndex(item => item.id === selectedItem.id);
+                if (removeIndex !== -1) {
+                    favItems.splice(removeIndex, 1);
+                    localStorage.setItem("favItems", JSON.stringify(favItems));
+                    hasAdded.classList.remove("fa-solid");
+                    hasAdded.classList.add("fa-regular");
+                    
+                    Swal.fire({
+                        icon: 'success',
+                        title: `${selectedItem.title} removed from favorites`,
+                    });
+                }
+            }
+        });
+    }
+    
+    
+
+    // adding to cart
+    let addBtns = document.querySelectorAll(".addBtn");
+    for (let btn of addBtns) {
+        btn.addEventListener("click", function (e) {
+            e.preventDefault();
+            let selectedItem = data[btn.name];
+
+            // local 
+            let cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+            cartItems.push(selectedItem);
+            localStorage.setItem("cartItems", JSON.stringify(cartItems));
+            Swal.fire({
+                icon: 'success',
+                title: `${selectedItem.title} added to cart`,
+            });
+
+
+
+            //removing from cart
+            let remBtns = document.querySelectorAll(".remCartBtn");
+            for (let btn of remBtns) {
+                btn.addEventListener("click", function () {
+                    Swal.fire({
+                        title: 'Are you sure want to remove item from cart?',
+                        icon: 'warning',
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'Yes, remove item.'
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            Swal.fire({
+                                icon: 'success',
+                                text: 'Item removed'
+                            }
+                            )
+                            sum -= Number(this.parentElement.querySelector('.price').textContent.replace('$', ''));
+                            cartHead.innerHTML = `<h3>Cart: $${sum.toFixed(2)} </h3>`;
+                            //   if(sum=="0.00"){
+                            //     cartHead.innerHTML = `<h3>Cart</h3>`;
+                            // }
+                            this.parentElement.remove()
+
+                        }
+                    })
+
+
+
+                })
+            }
+
+        })
+    }
+
+    //info 
+    let cardClicks = document.querySelectorAll(".card");
+    cardClicks.forEach((card) => {
+        card.addEventListener("click", function (e) {
+            e.preventDefault();
+            if (!e.target.closest(".favBtn") && !e.target.closest(".addBtn")) {
+                Swal.fire({
+                    title: data[this.querySelector(".addBtn").name].title,
+                    text: `${data[this.querySelector(".addBtn").name].description} Rated with ${data[this.querySelector(".addBtn").name].rating.rate}/5, you can get this item for $${data[this.querySelector(".addBtn").name].price} `,
+                    imageUrl: data[this.querySelector(".addBtn").name].image,
+                    imageWidth: 250,
+                    imageHeight: 200,
+                    imageAlt: 'Custom image',
+                });
+            }
+        });
+    });
+}
+let fetcherLHP = function () {
+    products.innerHTML = "";
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            data.sort((a, b) => a.price - b.price);
+            creator(data);
+
+        }).catch()
+}
+let fetcherHLP = function () {
+    products.innerHTML = "";
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            data.sort((a, b) => b.price - a.price);
+            creator(data);
+        }).catch()
+}
+let fetcherLHR = function () {
+    products.innerHTML = "";
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            data.sort((a, b) => a.rating?.rate - b.rating?.rate);
+            creator(data);
+
+        }).catch();
+}
+let fetcherHLR = function () {
+    products.innerHTML = "";
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            data.sort((a, b) => b.rating?.rate - a.rating?.rate);
+            creator(data);
+
+        }).catch()
+}
+
+//default load
+preFetch()
+//all
+all.addEventListener("click", function () {
+    preFetch()
+})
+//men
+men.addEventListener("click", function () {
+    fetcher(mens)
+});
+//women
+women.addEventListener("click", function () {
+    fetcher(womens)
+});
+//jewelery
+acc.addEventListener("click", function () {
+    fetcher(jewelery)
+});
+//electronics
+elecs.addEventListener("click", function () {
+    fetcher(electronics)
+});
+//select option
+filters.addEventListener("change", function () {
+    let selectedOption = filters.options[filters.selectedIndex].value;
+    switch (selectedOption) {
+        case "lowToHighPrice":
+            fetcherLHP()
+            break;
+        case "highToLowPrice":
+            fetcherHLP()
+            break;
+        case "lowToHighRating":
+            fetcherLHR()
+            break;
+        case "highToLowRating":
+            fetcherHLR()
+            break;
+        default:
+            break;
+    }
+});
+
+                
+
